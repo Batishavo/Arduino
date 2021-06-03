59d28b3cba529dcd52937d959e3a16af211ef787
Carlos Raúl Romero Martínez <45341513+Batishavo@users.noreply.github.com>
2021-06-02 21:30:56 -0500

    Add files via upload

---
+++ LedVerde/LedVerde.ino
@@ -0,0 +1,20 @@
+int contador=0;
+boolean b1=0,b2=0,led=0;
+void setup() {
+  pinMode(12, OUTPUT);
+  pinMode(7,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  b2=digitalRead(7);
+  if((b1^b2)&(!b2)){
+    contador++;
+  }
+  if(contador>=5){
+    led=!led;
+    contador=0;  
+  }
+  digitalWrite(12,led);
+  b1=b2;
+}
